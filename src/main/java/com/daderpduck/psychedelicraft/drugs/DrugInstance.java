e6f473c56637941f4837bd6e9781cfb683f8710a
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-03-30 15:12:02 -0700

    Slow down decay, fix desired effect never reaching 0 :p

--- src/main/java/com/daderpduck/psychedelicraft/drugs/DrugInstance.java
+++ src/main/java/com/daderpduck/psychedelicraft/drugs/DrugInstance.java
@@ -41,7 +41,8 @@ public class DrugInstance {
         } else {
             this.currentEffect += (desiredEffect - currentEffect)/5F;
             this.currentEffect = MathHelper.clamp(this.currentEffect, 0, drug.getMaxEffect());
-            this.desiredEffect -= desiredEffect/30F;
+            this.desiredEffect -= desiredEffect/3000F;
+            if (this.desiredEffect < 0.1F) this.desiredEffect = 0;
             this.desiredEffect = Math.max(desiredEffect, 0);
         }
     }
