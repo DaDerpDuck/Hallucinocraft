66378d612e3b4a163861061ef2bc2010fc6ccadd
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-03-30 16:41:47 -0700

    Reveal your secrets, SheetData

---
+++ src/main/java/com/daderpduck/psychedelicraft/mixin/client/AccessorSheetData.java
@@ -0,0 +1,14 @@
+package com.daderpduck.psychedelicraft.mixin.client;
+
+import net.minecraft.client.renderer.texture.AtlasTexture;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(AtlasTexture.SheetData.class)
+public interface AccessorSheetData {
+    @Accessor
+    int getWidth();
+
+    @Accessor
+    int getHeight();
+}
