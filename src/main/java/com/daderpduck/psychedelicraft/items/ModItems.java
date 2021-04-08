c27acbfb94f7547802caff8792054fceefa14547
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-04-08 02:57:39 -0700

    Extended duration of red shrooms

--- src/main/java/com/daderpduck/psychedelicraft/items/ModItems.java
+++ src/main/java/com/daderpduck/psychedelicraft/items/ModItems.java
@@ -7,7 +7,7 @@ import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
 
 public class ModItems {
-    public static final RegistryObject<Item> RED_SHROOMS = Psychedelicraft.ITEMS.register("red_shrooms", () -> new DrugItem(new DrugItem.Properties().addDrug(DrugRegistry.RED_SHROOMS, 400, 0.3F, 6000).edible().tab(ItemGroup.TAB_MATERIALS)));
+    public static final RegistryObject<Item> RED_SHROOMS = Psychedelicraft.ITEMS.register("red_shrooms", () -> new DrugItem(new DrugItem.Properties().addDrug(DrugRegistry.RED_SHROOMS, 400, 0.3F, 9600).edible().tab(ItemGroup.TAB_MATERIALS)));
 
     public static void register() {}
 }
