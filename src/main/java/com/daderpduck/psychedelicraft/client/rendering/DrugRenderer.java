c3ab053a69c4d47b87dddeff7debff1ba4c2e08d
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-04-11 00:41:33 -0700

    Merge EventHandler into DrugRenderer

--- src/main/java/com/daderpduck/psychedelicraft/client/rendering/DrugRenderer.java
+++ src/main/java/com/daderpduck/psychedelicraft/client/rendering/DrugRenderer.java
@@ -1,10 +1,16 @@
 package com.daderpduck.psychedelicraft.client.rendering;
 
 import com.daderpduck.psychedelicraft.Psychedelicraft;
+import com.daderpduck.psychedelicraft.client.rendering.shaders.RenderUtil;
+import com.daderpduck.psychedelicraft.client.rendering.shaders.ShaderRenderer;
 import com.daderpduck.psychedelicraft.drugs.Drug;
+import com.daderpduck.psychedelicraft.events.hooks.RenderBlockEntityEvent;
+import com.daderpduck.psychedelicraft.events.hooks.RenderEntityEvent;
+import com.daderpduck.psychedelicraft.events.hooks.RenderTerrainEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -34,5 +40,59 @@ public class DrugRenderer {
                 effect.resetValue();
             }
         }
+    }
+
+    // Shader stuff
+
+    @SubscribeEvent
+    public static void onRenderTerrain(RenderTerrainEvent event) {
+        if (event.phase == RenderTerrainEvent.Phase.START) {
+            ShaderRenderer.getWorldShader().safeGetUniform("lightmapEnabled").setInt(1);
+            ShaderRenderer.startRenderPass();
+        } else {
+            ShaderRenderer.endRenderPass();
+        }
+
+        RenderUtil.checkGlErrors();
+    }
+
+    @SubscribeEvent
+    public static void onRenderEntity(RenderEntityEvent event) {
+        if (event.phase == RenderEntityEvent.Phase.START) {
+            ShaderRenderer.startRenderPass();
+        } else {
+            ShaderRenderer.endRenderPass();
+        }
+
+        RenderUtil.checkGlErrors();
+    }
+
+    @SubscribeEvent
+    public static void onRenderBlockEntity(RenderBlockEntityEvent event) {
+        if (event.phase == RenderBlockEntityEvent.Phase.START) {
+            ShaderRenderer.startRenderPass();
+        } else {
+            ShaderRenderer.endRenderPass();
+        }
+
+        RenderUtil.checkGlErrors();
+    }
+
+    //TODO: Hook into block outline
+    /*@SubscribeEvent
+    public static void onRenderBlockOutline(RenderBlockOutlineEvent event) {
+        if (event.phase == RenderBlockOutlineEvent.Phase.START) {
+            RenderUtil.flushRenderBuffer();
+            startRenderPass(shaderWorld);
+        } else {
+            endRenderPass();
+        }
+
+        RenderUtil.checkGlErrors();
+    }*/
+
+    @SubscribeEvent
+    public static void renderPostWorld(RenderWorldLastEvent event) {
+        ShaderRenderer.processPostShaders(event.getPartialTicks());
     }
 }
