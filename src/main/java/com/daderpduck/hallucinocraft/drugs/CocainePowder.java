c94db38fc30d7d12878138929d5543a78895fbb9
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-04-12 10:52:05 -0700

    Add cocaine powder

---
+++ src/main/java/com/daderpduck/hallucinocraft/drugs/CocainePowder.java
@@ -0,0 +1,22 @@
+package com.daderpduck.hallucinocraft.drugs;
+
+import net.minecraft.entity.player.PlayerEntity;
+
+public class CocainePowder extends Drug {
+    public CocainePowder(DrugProperties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void renderTick(float effect) {
+        DrugEffects.BRIGHTNESS.addValue(effect*0.3F);
+        DrugEffects.SATURATION.addValue(effect*-0.2F);
+        DrugEffects.CAMERA_TREMBLE.addValue(effect);
+    }
+
+    @Override
+    public void effectTick(PlayerEntity player, float effect) {
+        DrugEffects.MOVEMENT_SPEED.addValue(effect*0.5F);
+        DrugEffects.DIG_SPEED.addValue(effect*0.5F);
+    }
+}
