7dd363bb061cf4e2df0de8839b4f006264f5a122
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-04-13 18:42:14 -0700

    Make potency represent a percentage of max effect

--- src/main/java/com/daderpduck/hallucinocraft/items/ModItems.java
+++ src/main/java/com/daderpduck/hallucinocraft/items/ModItems.java
@@ -12,10 +12,10 @@ public class ModItems {
     public static final RegistryObject<Item> BROWN_SHROOMS = register("brown_shrooms", () -> new DrugItem(new DrugItem.Properties().addDrug(DrugRegistry.BROWN_SHROOMS, 200, 0.3F, 3200).edible().tab(Hallucinocraft.TAB)));
     public static final RegistryObject<Item> DRIED_RED_MUSHROOM = register("dried_red_mushroom", () -> new Item(new Item.Properties().tab(Hallucinocraft.TAB)));
     public static final RegistryObject<Item> DRIED_BROWN_MUSHROOM = register("dried_brown_mushroom", () -> new Item(new Item.Properties().tab(Hallucinocraft.TAB)));
-    public static final RegistryObject<Item> COCAINE_POWDER = register("cocaine_powder", () -> new SnortDrugItem(new DrugItem.Properties().addDrug(DrugRegistry.COCAINE, 100, 0.6F, 3200).edible().tab(Hallucinocraft.TAB)));
+    public static final RegistryObject<Item> COCAINE_POWDER = register("cocaine_powder", () -> new SnortDrugItem(new DrugItem.Properties().addDrug(DrugRegistry.COCAINE, 100, 0.3F, 3200).edible().tab(Hallucinocraft.TAB)));
     public static final RegistryObject<Item> COCA_LEAF = register("coca_leaf", () -> new Item(new Item.Properties().tab(Hallucinocraft.TAB)));
     public static final RegistryObject<Item> COCA_MULCH = register("coca_mulch", () -> new Item(new Item.Properties().tab(Hallucinocraft.TAB)));
-    public static final RegistryObject<Item> COCAINE_DUST = register("cocaine_dust", () -> new SnortDrugItem(new DrugItem.Properties().addDrug(DrugRegistry.COCAINE, 100, 0.1F, 400).edible().tab(Hallucinocraft.TAB)));
+    public static final RegistryObject<Item> COCAINE_DUST = register("cocaine_dust", () -> new SnortDrugItem(new DrugItem.Properties().addDrug(DrugRegistry.COCAINE, 100, 0.01F, 400).edible().tab(Hallucinocraft.TAB)));
 
     public static RegistryObject<Item> register(String name, Supplier<Item> supplier) {
         return Hallucinocraft.ITEMS.register(name, supplier);
