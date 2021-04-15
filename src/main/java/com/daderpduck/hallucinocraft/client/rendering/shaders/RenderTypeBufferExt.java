770aac7cabc550ea54c0f195a29f3af567b49343
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-04-15 08:38:51 -0700

    Fix optifine crash

---
+++ src/main/java/com/daderpduck/hallucinocraft/client/rendering/shaders/RenderTypeBufferExt.java
@@ -0,0 +1,5 @@
+package com.daderpduck.hallucinocraft.client.rendering.shaders;
+
+public interface RenderTypeBufferExt {
+    void flushRenderBuffers();
+}
--- src/main/java/com/daderpduck/hallucinocraft/client/rendering/shaders/RenderUtil.java
+++ src/main/java/com/daderpduck/hallucinocraft/client/rendering/shaders/RenderUtil.java
@@ -1,34 +1,44 @@
 package com.daderpduck.hallucinocraft.client.rendering.shaders;
 
 import com.daderpduck.hallucinocraft.Hallucinocraft;
-import com.daderpduck.hallucinocraft.mixin.client.AccessorRenderTypeBuffer;
+import com.daderpduck.hallucinocraft.mixin.client.InvokerRenderUtilsOF;
 import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.lwjgl.opengl.GL11;
 
 @OnlyIn(Dist.CLIENT)
 public class RenderUtil {
+    public static final boolean hasOptifine;
+
+    static {
+        boolean hasOptifine1;
+        try {
+            Class.forName("optifine.Utils");
+            hasOptifine1 = true;
+        } catch (ClassNotFoundException e) {
+            hasOptifine1 = false;
+        }
+        hasOptifine = hasOptifine1;
+    }
+
     public static void flushRenderBuffer() {
-        Minecraft mc = Minecraft.getInstance();
-        flushRenderBuffer(mc.renderBuffers().bufferSource());
-        flushRenderBuffer(mc.renderBuffers().crumblingBufferSource());
+        if (hasOptifine) {
+            boolean old = InvokerRenderUtilsOF.callSetFlushRenderBuffers(true);
+            InvokerRenderUtilsOF.callFlushRenderBuffers();
+            InvokerRenderUtilsOF.callSetFlushRenderBuffers(old);
+        } else {
+            Minecraft mc = Minecraft.getInstance();
+            flushRenderBuffer(mc.renderBuffers().bufferSource());
+            flushRenderBuffer(mc.renderBuffers().crumblingBufferSource());
+        }
     }
 
     private static void flushRenderBuffer(IRenderTypeBuffer.Impl buffer) {
-        AccessorRenderTypeBuffer accessor = (AccessorRenderTypeBuffer) buffer;
-        RenderType renderType = null;
-        if (accessor.getLastState().isPresent()) {
-            renderType = accessor.getLastState().get();
-        }
-        buffer.endBatch();
-
-        if (renderType != null) {
-            buffer.getBuffer(renderType);
-        }
+        RenderTypeBufferExt bufferExt = (RenderTypeBufferExt) buffer;
+        bufferExt.flushRenderBuffers();
     }
 
     public static void checkGlErrors() {
