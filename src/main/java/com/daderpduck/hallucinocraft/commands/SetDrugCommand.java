740c360e173a3afc6a6c74eb4ee933796d76e437
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-04-20 12:49:38 -0700

    Remove debug shaders command

--- src/main/java/com/daderpduck/hallucinocraft/commands/SetDrugCommand.java
+++ src/main/java/com/daderpduck/hallucinocraft/commands/SetDrugCommand.java
@@ -1,17 +1,13 @@
 package com.daderpduck.hallucinocraft.commands;
 
 import com.daderpduck.hallucinocraft.capabilities.PlayerDrugs;
-import com.daderpduck.hallucinocraft.client.rendering.shaders.ShaderRenderer;
 import com.daderpduck.hallucinocraft.drugs.Drug;
 import com.daderpduck.hallucinocraft.drugs.DrugInstance;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.BoolArgumentType;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.command.arguments.EntityArgument;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.loading.FMLEnvironment;
 
 import java.util.Collection;
 
@@ -28,9 +24,6 @@ public class SetDrugCommand {
                     .then(Commands.literal("clear")
                         .then(Commands.argument("players", EntityArgument.players())
                             .executes(context -> clearDrug(context.getSource(), EntityArgument.getPlayers(context, "players")))))
-                    .then(Commands.literal("shaders")
-                         .then(Commands.argument("enabled", BoolArgumentType.bool())
-                            .executes(context -> toggleShaders(context.getSource(), BoolArgumentType.getBool(context, "enabled")))))
         );
     }
 
@@ -47,18 +40,6 @@ public class SetDrugCommand {
             Drug.clearDrugs(player);
             PlayerDrugs.sync(player);
         }
-        return 1;
-    }
-
-    private static int toggleShaders(CommandSource source, boolean enable) {
-        if (FMLEnvironment.dist == Dist.CLIENT)
-            if (ShaderRenderer.useShader != enable) {
-                if (enable) {
-                    ShaderRenderer.setup();
-                } else {
-                    ShaderRenderer.clear(false);
-                }
-            }
         return 1;
     }
 }
