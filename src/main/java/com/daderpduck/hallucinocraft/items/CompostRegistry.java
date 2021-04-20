a5453c75948002607b4243e35fb1edffa481672a
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-04-20 12:57:22 -0700

    Add cannabis seed to compost

--- src/main/java/com/daderpduck/hallucinocraft/items/CompostRegistry.java
+++ src/main/java/com/daderpduck/hallucinocraft/items/CompostRegistry.java
@@ -8,5 +8,6 @@ public class CompostRegistry {
         ComposterBlock.COMPOSTABLES.put(ModItems.COCA_SEEDS.get(), 0.3F);
         ComposterBlock.COMPOSTABLES.put(ModItems.CANNABIS_LEAF.get(), 0.3F);
         ComposterBlock.COMPOSTABLES.put(ModItems.CANNABIS_BUD.get(), 0.5F);
+        ComposterBlock.COMPOSTABLES.put(ModItems.CANNABIS_SEEDS.get(), 0.3F);
     }
 }
