87085afab2a046d2552e08a970ff4f38448b3b9e
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-04-24 00:37:51 -0700

    Fix bong using normal cannabis bud instead of dried

--- src/main/java/com/daderpduck/hallucinocraft/items/BongRegistry.java
+++ src/main/java/com/daderpduck/hallucinocraft/items/BongRegistry.java
@@ -5,6 +5,6 @@ import com.daderpduck.hallucinocraft.drugs.DrugRegistry;
 public class BongRegistry {
     public static void register() {
         BongItem.BONGABLES.put(ModItems.COCAINE_ROCK.get(), new ModItems.DrugChain().add(DrugRegistry.COCAINE, 0, 0.5F, 2800));
-        BongItem.BONGABLES.put(ModItems.CANNABIS_BUD.get(), new ModItems.DrugChain().add(DrugRegistry.CANNABIS, 0, 0.12F, 3200));
+        BongItem.BONGABLES.put(ModItems.DRIED_CANNABIS_BUD.get(), new ModItems.DrugChain().add(DrugRegistry.CANNABIS, 0, 0.12F, 3200));
     }
 }
