17755334b4ec656d4fc1f47c2bfbe27a96cfd54b
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-04-23 21:37:48 -0700

    Remove cocaine joint for bong

--- src/main/java/com/daderpduck/hallucinocraft/items/ModItems.java
+++ src/main/java/com/daderpduck/hallucinocraft/items/ModItems.java
@@ -21,21 +21,25 @@ import java.util.function.Supplier;
 public class ModItems {
     public static final RegistryObject<DrugItem> RED_SHROOMS = registerDrug("red_shrooms", new DrugChain().add(DrugRegistry.RED_SHROOMS, 400, 0.3F, 9600));
     public static final RegistryObject<Item> DRIED_RED_MUSHROOM = registerItem("dried_red_mushroom");
+
     public static final RegistryObject<DrugItem> BROWN_SHROOMS = registerDrug("brown_shrooms", new DrugChain().add(DrugRegistry.BROWN_SHROOMS, 200, 0.3F, 3200));
     public static final RegistryObject<Item> DRIED_BROWN_MUSHROOM = registerItem("dried_brown_mushroom");
-    public static final RegistryObject<DrugItem> COCAINE_JOINT = registerDrug("cocaine_joint", new DrugChain().add(DrugRegistry.COCAINE, 0, 0.5F, 2800), UseAction.BOW, 16);
+
+    public static final RegistryObject<Item> COKE_CAKE = registerBlock("coke_cake", ModBlocks.COKE_CAKE_BLOCK, 1);
     public static final RegistryObject<Item> COCAINE_ROCK = registerItem("cocaine_rock");
     public static final RegistryObject<DrugItem> COCAINE_POWDER = registerDrug("cocaine_powder", new DrugChain().add(DrugRegistry.COCAINE, 100, 0.3F, 3200), UseAction.BOW, 64);
     public static final RegistryObject<DrugItem> COCAINE_DUST = registerDrug("cocaine_dust", new DrugChain().add(DrugRegistry.COCAINE, 100, 0.05F, 1000), UseAction.BOW, 64);
     public static final RegistryObject<Item> COCA_MULCH = registerItem("coca_mulch");
     public static final RegistryObject<Item> COCA_LEAF = registerItem("coca_leaf");
     public static final RegistryObject<Item> COCA_SEEDS = registerBlockNamed("coca_seeds", ModBlocks.COCA_BLOCK);
+
     public static final RegistryObject<DrugItem> CANNABIS_JOINT = registerDrug("cannabis_joint", new DrugChain().add(DrugRegistry.CANNABIS, 0, 0.12F, 3200), UseAction.BOW, 16);
     public static final RegistryObject<Item> DRIED_CANNABIS_LEAF = registerItem("dried_cannabis_leaf");
     public static final RegistryObject<Item> CANNABIS_LEAF = registerItem("cannabis_leaf");
     public static final RegistryObject<Item> DRIED_CANNABIS_BUD = registerItem("dried_cannabis_bud");
     public static final RegistryObject<Item> CANNABIS_SEEDS = registerBlockNamed("cannabis_seeds", ModBlocks.CANNABIS_BLOCK);
     public static final RegistryObject<Item> CANNABIS_BUD = registerItem("cannabis_bud");
+
     public static final RegistryObject<Item> EMPTY_SYRINGE = registerItem("syringe", 16);
     public static final RegistryObject<SyringeItem> COCAINE_SYRINGE = registerSyringe("cocaine_syringe", new DrugChain().add(DrugRegistry.COCAINE, 0, 0.5F, 4800), 0xFFFFFFFF);
     public static final RegistryObject<Item> COKE_CAKE = registerBlock("coke_cake", ModBlocks.COKE_CAKE_BLOCK, 1);
@@ -88,7 +92,7 @@ public class ModItems {
         return Hallucinocraft.ITEMS.register(name, supplier);
     }
 
-    private static class DrugChain {
+    public static class DrugChain {
         private final List<DrugEffectProperty> list = new ArrayList<>();
 
         public DrugChain add(RegistryObject<Drug> drug, int delayTicks, float potencyPercentage, int duration) {
@@ -114,6 +118,7 @@ public class ModItems {
     @OnlyIn(Dist.CLIENT)
     public static void registerItemColors(ColorHandlerEvent.Item event) {
         event.getItemColors().register(new SyringeItem.Color(), COCAINE_SYRINGE.get());
+        event.getItemColors().register(new BongItem.Color(), BONG.get());
     }
 
     public static void init() {
