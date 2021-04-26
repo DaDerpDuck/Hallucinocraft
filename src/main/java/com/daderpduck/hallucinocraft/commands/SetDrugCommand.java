fb70b0f29996776297919cd71e5a6e172f583e72
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-04-26 14:59:34 -0700

    Add potency and duration argument to drug command

--- src/main/java/com/daderpduck/hallucinocraft/commands/SetDrugCommand.java
+++ src/main/java/com/daderpduck/hallucinocraft/commands/SetDrugCommand.java
@@ -4,6 +4,8 @@ import com.daderpduck.hallucinocraft.capabilities.PlayerDrugs;
 import com.daderpduck.hallucinocraft.drugs.Drug;
 import com.daderpduck.hallucinocraft.drugs.DrugInstance;
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.FloatArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.command.arguments.EntityArgument;
@@ -20,16 +22,20 @@ public class SetDrugCommand {
                     .then(Commands.literal("give")
                         .then(Commands.argument("players", EntityArgument.players())
                             .then(Commands.argument("drug", new DrugArgument())
-                                .executes(context -> setDrugValue(context.getSource(), EntityArgument.getPlayers(context, "players"), DrugArgument.getDrug(context, "drug"))))))
+                                .executes(context -> setDrugValue(context.getSource(), EntityArgument.getPlayers(context, "players"), DrugArgument.getDrug(context, "drug"), 0.5F, 1200))
+                                .then(Commands.argument("potency", FloatArgumentType.floatArg(0, 1))
+                                    .executes(context -> setDrugValue(context.getSource(), EntityArgument.getPlayers(context, "players"), DrugArgument.getDrug(context, "drug"), FloatArgumentType.getFloat(context, "potency"), 1200))
+                                    .then(Commands.argument("duration", IntegerArgumentType.integer(0))
+                                        .executes(context -> setDrugValue(context.getSource(), EntityArgument.getPlayers(context, "players"), DrugArgument.getDrug(context, "drug"), FloatArgumentType.getFloat(context, "potency"), IntegerArgumentType.getInteger(context, "duration"))))))))
                     .then(Commands.literal("clear")
                         .then(Commands.argument("players", EntityArgument.players())
                             .executes(context -> clearDrug(context.getSource(), EntityArgument.getPlayers(context, "players")))))
         );
     }
 
-    private static int setDrugValue(CommandSource source, Collection<ServerPlayerEntity> players, Drug drug) {
+    private static int setDrugValue(CommandSource source, Collection<ServerPlayerEntity> players, Drug drug, float potency, int duration) {
         for (ServerPlayerEntity player : players) {
-            Drug.addDrug(player, new DrugInstance(drug, 0, 10F, 1200));
+            Drug.addDrug(player, new DrugInstance(drug, 0, potency, duration));
             PlayerDrugs.sync(player);
         }
         return 1;
