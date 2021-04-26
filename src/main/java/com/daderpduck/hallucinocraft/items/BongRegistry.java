2005b96969ffe24356f72f55c4a1af5ca0a6a987
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2021-04-26 09:03:40 -0700

    Slightly decrease crack potency

--- src/main/java/com/daderpduck/hallucinocraft/items/BongRegistry.java
+++ src/main/java/com/daderpduck/hallucinocraft/items/BongRegistry.java
@@ -4,7 +4,7 @@ import com.daderpduck.hallucinocraft.drugs.DrugRegistry;
 
 public class BongRegistry {
     public static void register() {
-        BongItem.BONGABLES.put(ModItems.COCAINE_ROCK.get(), new ModItems.DrugChain().add(DrugRegistry.COCAINE, 0, 0.5F, 2800));
+        BongItem.BONGABLES.put(ModItems.COCAINE_ROCK.get(), new ModItems.DrugChain().add(DrugRegistry.COCAINE, 0, 0.45F, 2800));
         BongItem.BONGABLES.put(ModItems.DRIED_CANNABIS_BUD.get(), new ModItems.DrugChain().add(DrugRegistry.CANNABIS, 0, 0.2F, 3200));
     }
 }
